68e0dbdb00bf9d5b93f8470a2ce78bcf9e764f04
barbi-busquier <163878591+barbi-busquier@users.noreply.github.com>
2024-05-15 14:12:48 -0300

    Add files via upload

---
+++ constantes.py
@@ -0,0 +1,12 @@
+""" constantes de la app """
+
+DB_HOSTNAME="127.0.0.1"
+DB_USERNAME="root"
+DB_PASSWORD=""
+DB_DATABASE="wordle"
+
+DB_TABLES=[
+    'jugadas',
+    'jugadores',
+    'palabras'
+]
