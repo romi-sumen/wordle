a0ea2a267ac1d59909e7f2f2670a2b237b202e5e
5i7 <5i7@cangallo.escuela>
2024-06-03 11:51:15 -0300

    se agrega funcion comprar_palabras_existe

--- test_wordle.py
+++ test_wordle.py
@@ -7,8 +7,12 @@ from constantes import DB_TABLES
 def test_verificar_letra_existe():
     at_wordle = dir(wordle)
     assert 'verificar_letra' in at_wordle
-    
-@pytest.mark.usefixtures("conn_fixture")
-def test_verificar_letra_resultado():
-    resultado = wordle.verificar_letra(pytest.dbconn, 1)
-    assert resultado[1] == ("c", "abaca")
+
+#def test_verificar_letra_resultado():
+#    resultado = wordle.verificar_letra("abaca", "c")
+#    estimado = [3]
+#    assert resultado == estimado
+
+def test_verificar_comparar_palabras_existe():
+    at_wordle = dir(wordle)
+    assert 'comparar_palabras' in  at_wordle
--- wordle.py
+++ wordle.py
@@ -7,7 +7,7 @@ from constantes import DB_HOSTNAME,DB_DATABASE,DB_PASSWORD,DB_USERNAME
 #a)Verificar si una letra está dentro de una palabra devolviendo una lista 
 # con las posiciones donde se encuentra la misma o lista vacía en caso de no encontrarse.
 
-def verificar_letra(conn : db.MySQLConnection, palabra : str, letra : chr):
+def verificar_letra(palabra : str, letra : chr):
     posiciones = []
     for i in range(len(palabra)):
         if palabra[i] == letra:
@@ -18,6 +18,14 @@ def verificar_letra(conn : db.MySQLConnection, palabra : str, letra : chr):
 # letra está o no en la palabra y si está en la posición correcta (puede utilizar una lista, 
 # un formato de string (str) o cualquier otro recurso que desee). 
 # Tenga en cuenta que ahora cuenta con letras repetidas en el diccionario. 
+
+def comparar_palabras (palabra_jugador : str, intento : str):
+    resultado = []
+    for i in range(len(palabra_jugador)):
+        if palabra_jugador [i] == intento[i]: resultado.append('X')
+        elif palabra_jugador[i] in intento: resultado.append('O')
+        else: resultado.append('_')
+        return resultado
 
 #c)Recibe el nombre de un jugador y devuelve una palabra y su id (en formato lista o tupla) 
 # que no haya jugado (puede que sea la primera vez que juega o no)
